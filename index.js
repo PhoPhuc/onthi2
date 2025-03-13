e66644bfaa4e664c7d7d0050d707161257741615
PhoPhuc <phucvuaht@gmail.com>
2025-03-13 20:55:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-document.getElementById('closeButton').addEventListener('click', function() {
-  var notification = document.getElementById('notification');
-  notification.classList.add('closing');
 
-  notification.addEventListener('animationend', function() {
-    notification.style.display = 'none';
-  }, { once: true });
-});
